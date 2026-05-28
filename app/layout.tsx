6e8979508263a8417cf778a6dfd9fcbb7f57d126
mohiteight123-wq <mohit.eight.123@gmail.com>
2026-05-28 20:54:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
-export const metadata = {
-  title: "Vertical TV Pipeline",
-};
-
 export default function RootLayout({ children }: any) {
+  return (
     <html lang="en">
       <body>{children}</body>
     </html>
